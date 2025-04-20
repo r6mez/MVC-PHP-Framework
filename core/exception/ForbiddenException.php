73e75c4fa00db4d5eb3f0448053716ceb9f321f3
Ramez Medhat <iramezdev@gmail.com>
2025-04-20 14:43:49 +0200

    Remove core components: delete Application, Controller, Database, Model, Request, Response, View, and middleware classes; update composer files to reflect changes.

--- core/exception/ForbiddenException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Core\Exception;
-
-class ForbiddenException extends \Exception {
-    protected $message = "You do not have permission to access access this page";
-    protected $code = 403;
-}
--- core/exception/NotFoundException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Core\Exception;
-
-class NotFoundException extends \Exception {
-    protected $message = "Ops! You're lost";
-    protected $code = 404;
-}
